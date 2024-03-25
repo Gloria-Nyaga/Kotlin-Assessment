c3f70f8c36f06522146a938446dce6423d1094a5
Gloria-Nyaga <nyagagakiigloria@gmail.com>
2024-03-25 08:23:53 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,54 @@
+fun main() {
+ numbers(10,20,30,40)
+    var sum =(10 +20 +30 +40);
+    println (sum)
+
+}
+
+// Write and invoke a function that given the string"Barnie bakes brown bagels and buns" prints out a list of all the words and characters making up the string excluding all possible occurrences of the letter b.
+fun statement(sentence:String){
+    var sentence ="Barnie bakes brown bagels and buns";
+
+}
+
+
+
+
+
+
+
+
+
+//Write and invoke a function that takes in an array of integers and returns 3 values: sum, count and average of all elements.
+
+fun numbers(num1:Int,num2:Int, num3:Int,num4:Int):Int{
+    var sum =(num1 +num2 +num3 +num4);
+    return sum;
+
+    var average=()
+
+}
+
+
+
+//The volume of a sphere is calculated using the formula below
+
+fun numerics(radius:Int,pie:Int, number:Int): Int{
+
+}
+
+
+//
+fun isPalindrome(word:String):Boolean{
+    var word = "Hannah";
+    if {
+        (word == "palindrome")
+        println (true)
+    }
+         else{
+        println(false)
+    }
+
+
+}
+
